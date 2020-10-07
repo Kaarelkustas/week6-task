384b32e6ce42e8a21756448676ed672bff135034
Opilane TTHK <opilane@tthk.ee>
2020-10-07 16:37:33 +0300

    commit

---
+++ week6-task/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace week6_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] numbers = new int[10];
+
+            for(int i = 0; i <numbers.Lenght; i++)
+            {
+                numbers[i] = rnd.Next(1, 11);
+            }
+                int sum = 0;  
+              foreach(int num in numbers)
+            {
+                sum = sum + num;
+                Console.WriteLine(num);
+            }
+            Console.WriteLine($"total: {sum}");
+        }   
+    }
+}
